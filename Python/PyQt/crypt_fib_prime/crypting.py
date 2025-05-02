a6c2746ddbc50b2b436b9ebc680d353d229f9f02
NT-000 <nicolai.thue.reier@gmail.com>
2025-05-02 20:40:15 +0200

    Crypt with Vigenere-crypting, decrypt,  crack code without key, find closest prime - and fibonacci number based on user input. Implemented everything in a desktop app using PyQt, only bare basics.

---
+++ Python/PyQt/crypt_fib_prime/crypting.py
@@ -0,0 +1,245 @@
+import sys
+from logging import exception
+from warnings import catch_warnings
+
+import requests
+from urllib.parse import quote
+from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow, QPushButton, QLabel,QMessageBox
+from PyQt5.QtCore import QCoreApplication, Qt
+from PyQt5.QtGui import QIntValidator
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Vigenère Kryptering, nærmeste prim - og fibonacci-tall")
+        self.setGeometry(1000, 100, 800, 1200)
+        self.input = QLineEdit(self)
+        self.ciphered_text = QLineEdit(self)
+        self.key_input = QLineEdit(self)
+        self.closest_prime_input = QLineEdit(self)
+        self.closest_fibonacci_input = QLineEdit(self)
+
+        self.button = QPushButton("Krypter", self)
+        self.decrypt_button = QPushButton("Dekrypter", self)
+        self.crack_button = QPushButton("Knekk koden", self)
+        self.output_label = QLabel("Resultat:", self)
+        self.result_label = QLabel("", self)
+
+        self.initUI()
+
+    def initUI(self):
+        # input text to encrypt
+        self.input.setGeometry(150, 100, 200, 50)
+        self.input.setPlaceholderText("Skriv tekst her")
+        self.input.setStyleSheet("font-size: 20px;")
+        self.input.returnPressed.connect(self.encrypt_text)
+
+        # label input
+        input_label = QLabel("Tekst:", self)
+        input_label.setGeometry(50, 110, 100, 30)
+        input_label.setStyleSheet("font-size: 16px;")
+
+        # label key
+        key_label = QLabel("Nøkkel:", self)
+        key_label.setGeometry(50, 170, 100, 30)
+        key_label.setStyleSheet("font-size: 16px;")
+
+        # textinput key
+        self.key_input.setGeometry(150, 170, 200, 50)
+        self.key_input.setPlaceholderText("Skriv nøkkel her")
+        self.key_input.setStyleSheet("font-size: 20px;")
+
+        # label crypt text label
+
+        crypt_label = QLabel("Kryptert:", self)
+        crypt_label.setGeometry(50, 250, 100, 30)
+        crypt_label.setStyleSheet("font-size: 16px;")
+
+        # crypt input
+        self.ciphered_text.setStyleSheet("font-size: 20px;")
+        self.ciphered_text.setPlaceholderText("Kryptert tekst vil bli vist her")
+        self.ciphered_text.setGeometry(150, 240, 200, 50)
+
+
+        #primenum label
+        primenum_label = QLabel("Primtall:", self)
+        primenum_label.setGeometry(400, 110, 100, 30)
+        primenum_label.setStyleSheet("font-size: 16px;")
+
+        # primenum input
+        self.closest_prime_input.setValidator(QIntValidator())
+        self.closest_prime_input.setGeometry(500, 100, 200, 50)
+        self.closest_prime_input.setPlaceholderText("Finn nærmeste primtall")
+        self.closest_prime_input.setStyleSheet("font-size: 20px;")
+        self.closest_prime_input.returnPressed.connect(self.find_closest_prime)
+        # set range later
+
+        #fibonacci label
+        fibonacci_label = QLabel("Fibonacci-tall:", self)
+        fibonacci_label.setGeometry(400, 170, 110, 30)
+        fibonacci_label.setStyleSheet("font-size: 16px;")
+
+        #fibonacci input
+        self.closest_fibonacci_input.setValidator(QIntValidator())
+        self.closest_fibonacci_input.setGeometry(500, 170, 200, 50)
+        self.closest_fibonacci_input.setPlaceholderText("Finn nærmeste fibonacci-tall")
+        self.closest_fibonacci_input.setStyleSheet("font-size: 20px;")
+        self.closest_fibonacci_input.returnPressed.connect(self.find_closest_fibonacci)
+        #Set range
+
+        # crypt-button
+        self.button.setGeometry(100, 300, 150, 50)
+        self.button.clicked.connect(self.encrypt_text)
+        self.button.setStyleSheet("background-color: red; color: white; font-weight: bold;")
+
+        # decrypt-button
+        self.decrypt_button.setGeometry(270, 300, 150, 50)
+        self.decrypt_button.clicked.connect(self.decrypt_text)
+        self.decrypt_button.setStyleSheet("background-color: blue; color: white; font-weight: bold;")
+
+        # crack-button
+        self.crack_button.setGeometry(440, 300, 160, 50)
+        self.crack_button.clicked.connect(self.crack_cipher)
+        self.crack_button.setStyleSheet("background-color: yellow; color: black; font-weight: bold;")
+
+        # label output
+        self.output_label.setGeometry(50, 500, 400, 30)
+        self.output_label.setStyleSheet("font-size: 16px;")
+
+        # result
+        self.result_label.setGeometry(50, 540, 700, 500)
+        self.result_label.setStyleSheet(
+            "font-size: 18px; background-color: #f0f0f0; padding: 10px; word-wrap: break-word;")
+        self.result_label.setWordWrap(True)
+
+    def find_closest_prime(self):
+        number = int(self.closest_prime_input.text())
+
+        if not number:
+            self.show_error("Vennligst skriv inn et tall")
+            return
+        prime_response = requests.get(f"http://127.0.0.1:8000/primes/closest/{number}")
+        prime_number = prime_response.json()["closest_prime"]
+        self.result_label.setText(f"Nærmeste primtall til {number} er: {prime_number}")
+        self.closest_prime_input.setText("")
+
+    def find_closest_fibonacci(self):
+        num = int(self.closest_fibonacci_input.text())
+
+        if not num:
+            self.show_error("Vennligst skriv inn et tall")
+            return
+
+        fibonacci_response = requests.get(f"http://127.0.0.1:8000/fibonacci/find_closest/{num}", timeout=10)
+        fibonacci_number = fibonacci_response.json()["closest_fibonacci_number"]
+        is_fibonacci = fibonacci_response.json()["is_fibonacci"]
+
+        if is_fibonacci:
+            self.result_label.setText(f"Tallet {num} er et fibonacci-tall")
+        else:
+            self.result_label.setText(f"Nærmeste fibonacci-tall til {num} er: {fibonacci_number}")
+
+        self.closest_fibonacci_input.setText("")
+
+    def encrypt_text(self):
+        text = self.input.text()
+        key = self.key_input.text()
+
+        if not text:
+            self.show_error("Vennligst skriv inn tekst å kryptere")
+            return
+
+        if not key:
+            self.show_error("Vennligst skriv inn en nøkkel for kryptering")
+            return
+
+        if not key.isalpha():
+            self.show_error("Nøkkelen kan bare inneholde bokstaver")
+            return
+
+
+        cipher_response = requests.get(
+           f"http://127.0.0.1:8000/cipher?text={quote(text)}&key={quote(key)}",
+             timeout=5
+            )
+
+        ciphered_text = cipher_response.json()["ciphered_text"]
+        self.result_label.setText(f"Kryptert tekst: {ciphered_text}")
+        self.ciphered_text.setText(ciphered_text)
+        self.input.setText("")
+        self.key_input.setText("")
+
+
+
+    def decrypt_text(self):
+        text = self.input.text()
+        key = self.key_input.text()
+
+        if not text:
+            self.show_error("Vennligst skriv inn tekst å dekryptere")
+            return
+
+        if not key:
+            self.show_error("Vennligst skriv inn en nøkkel for dekryptering")
+            return
+
+        if not key.isalpha():
+            self.show_error("Nøkkelen må bare inneholde bokstaver")
+            return
+
+
+        decipher_response = requests.get(
+            f"http://127.0.0.1:8000/decipher?ciphered_text={quote(text)}&key={quote(key)}",
+            timeout=5
+        )
+        decipher_response.raise_for_status()
+        deciphered_text = decipher_response.json()["deciphered_text"]
+        self.result_label.setText(f"Dekryptert tekst: {deciphered_text}")
+
+
+    def crack_cipher(self):
+        text = self.ciphered_text.text()
+
+        if not text:
+            self.show_error("Vennligst skriv inn kryptert tekst")
+            return
+
+        crack_response = requests.get(
+                f"http://127.0.0.1:8000/crack?ciphered_text={quote(text)}",
+                timeout=50
+            )
+
+        result = crack_response.json()
+
+        if result:
+            suggested_key = result.get("suggested_key", "")
+            deciphered_text = result.get("deciphered_text", "")
+
+            self.result_label.setText(
+                f"Sannsynlig nøkkel: {suggested_key}\n"
+                f"Nøkkellengde: {result.get('key_length', '')}\n\n"
+                f"Dekryptert tekst:\n\n{deciphered_text}"
+            )
+            self.key_input.setText(suggested_key)
+        else:
+            self.result_label.setText(
+                f"Kunne ikke knekke koden"
+            )
+
+
+    #error popup
+    def show_error(self, message):
+        error_dialog = QMessageBox(self)
+        error_dialog.setWindowTitle("Noe gikk feil")
+        error_dialog.setText(message)
+        error_dialog.setStandardButtons(QMessageBox.Ok)
+        error_dialog.exec_()
+#add error-handling
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ Python/PyQt/crypt_fib_prime/main.py
@@ -0,0 +1,257 @@
+import fastapi
+from collections import Counter
+import string
+
+import requests
+
+app = fastapi.FastAPI()
+
+"""english_frequencies = {
+    'A': 0.082, 'B': 0.015, 'C': 0.028, 'D': 0.043, 'E': 0.127, 'F': 0.022,
+    'G': 0.020, 'H': 0.061, 'I': 0.070, 'J': 0.002, 'K': 0.008, 'L': 0.040,
+    'M': 0.024, 'N': 0.067, 'O': 0.075, 'P': 0.019, 'Q': 0.001, 'R': 0.060,
+    'S': 0.063, 'T': 0.091, 'U': 0.028, 'V': 0.010, 'W': 0.023, 'X': 0.001,
+    'Y': 0.020, 'Z': 0.001
+}"""
+
+
+@app.get("/cipher")
+def vigenere_encrypt(text: str, key: str):
+    key = key.lower()
+    key_length = len(key)
+    result = []
+    key_index_position = 0
+
+    for char in text:
+        if char.isalpha():
+            key_letter = key[key_index_position % key_length]
+            shift = ord(key_letter) - ord('a')
+            shift_base = ord('A') if char.isupper() else ord('a')
+            encrypted_position = (ord(char) - shift_base + shift) % 26 # converts the char to pos(0-25), adds shift, converts back again
+            encrypted_char = chr(encrypted_position + shift_base)
+            result.append(encrypted_char)
+            key_index_position += 1
+        else:
+            result.append(char)  # stores characters that's not letters
+
+    return {"ciphered_text": ''.join(result)}
+
+@app.get("/decipher")
+def vigenere_decrypt(ciphered_text: str, key: str):
+
+    key = key.lower()
+    key_length = len(key)
+    result = []
+    key_index_position = 0
+
+    for char in ciphered_text:
+        if char.isalpha():
+            key_letter = key[key_index_position % key_length]
+            shift = ord(key_letter) - ord('a')
+            shift_base = ord('A') if char.isupper() else ord('a') # keep capitalization/small letters from the original text
+            original_position = (ord(char) - shift_base - shift + 26) % 26 # converts the char to pos(0-25), subtracts the shift and converts back - always a positive num
+            decrypted_char = chr(original_position + shift_base)
+            result.append(decrypted_char)
+            key_index_position += 1
+        else:
+            result.append(char) # stores characters that's not letters
+
+    return {"deciphered_text": ''.join(result)}
+
+@app.get("/crack")
+def crack_vigenere_cipher(ciphered_text: str):
+
+        # clean text for analyzing - keeps letters
+        cleaned_text = ''.join([c for c in ciphered_text.upper() if c.isalpha()])
+
+        if len(cleaned_text) < 20:
+            return {
+                "error": " Trenger minst 20 bokstaver."
+            }
+
+        # find probable key_length
+        key_length = find_key_length(cleaned_text)
+
+        # find
+        key = find_key(cleaned_text, key_length)
+
+        res = requests.get("/decipher")
+        decrypted = res.json()["deciphered_text"]
+
+        return {
+            "suggested_key": key.lower(),  # Tilpass til formatet brukt i frontend
+            "key_length": key_length,
+            "deciphered_text": decrypted
+        }
+
+@app.get("/primes/closest/{number}")
+async def get_closest_prime(number: int):
+    #generates numbers close to input number, have set an interval og 100, but can
+    upper_limit = number + 100
+    primes = []
+    numbers = [True] * (upper_limit + 1)
+    for i in range(2, upper_limit + 1):
+        if numbers[i]:
+            primes.append(i)
+            for j in range(i * i, upper_limit + 1, i):
+                numbers[j] = False
+
+    # find closest primenumber
+    min_distance = float('inf') #to be sure that min_distance is lesser than input
+    closest_prime = None
+
+    for p in primes:
+        distance = abs(number - p)
+        if distance < min_distance:
+            min_distance = distance
+            closest_prime = p
+
+    return {
+        "closest_prime": closest_prime,
+        "distance": min_distance
+    }
+
+@app.get("/fibonacci/find_closest/{num}")
+async def find_closest_fibonacci(num: int):
+    # generate Fibonacci numbers up to a reasonable limit
+    fibonacci_numbers = [0, 1]
+    while fibonacci_numbers[-1] < max(num * 2, 1000):  # Generate enough numbers
+        fibonacci_numbers.append(fibonacci_numbers[-1] + fibonacci_numbers[-2])
+
+    if num in fibonacci_numbers:
+        return {
+            "is_fibonacci": True,
+            "closest_fibonacci_number": num
+        }
+
+    def calculate_distance(x):
+        return abs(x - num)
+    # closest fib number, calculates distance to fib num abs always returns a positive number
+    closest_fib = min(fibonacci_numbers, key=calculate_distance)
+
+    return {
+        "is_fibonacci": False,
+        "closest_fibonacci_number": closest_fib
+    }
+
+
+def index_of_coincidence(text):
+    #calculates coincidence of a text
+    counts = Counter(text)
+    total = len(text)
+
+    if total <= 1:
+        return 0
+
+    ioc = 0
+    for count in counts.values():
+        ioc += count * (count - 1)
+
+    ioc /= total * (total - 1)
+    return ioc
+
+
+def find_key_length(ciphertext, max_length=20):
+
+    #finds the likely key length for a Vigenère-encrypted text
+    #using Index of Coincidence (IoC).
+
+    results = [] # stores a tuple(test_length, average_length)
+
+    # key lengths from 1 to max_length (or half of text-length)
+    max_potential_length = min(max_length, len(ciphertext) // 2)
+
+    for test_length in range(1, max_potential_length + 1):
+        # for each potential key length, create test_length number of groups
+        groups = []
+
+        # create groups by taking every test_length-th character
+
+        for start_pos in range(test_length):
+
+            # create a group with characters that share the same position in the key cycle
+            group = ""
+            pos = start_pos
+            while pos < len(ciphertext): # each character will be encrypted with the same char in the key, example if the key consist of 5 characters, each fifth char in the ciphertext will be encrypted with the same letter. If the key has a length of 5 it gets 5 different groups.
+                group += ciphertext[pos]
+                pos += test_length
+
+            groups.append(group)
+
+        # calculate average IoC for all groups
+        total_ioc = 0
+        for group in groups:
+            total_ioc += index_of_coincidence(group)
+
+        average_ioc = total_ioc / test_length
+
+        # store result for this key length
+        results.append((test_length, average_ioc))
+
+    # sort results based on how close the IoC value is to 0.067.
+    # the closer to 0.067, the more likely it is the correct key length
+
+    def sort_by_distance_to_target(x):
+        target_value = 0.067
+        return abs(x[1] - target_value)
+
+    results.sort(key=sort_by_distance_to_target)
+
+    # return the length with IoC value closest to 0.067
+    return results[0][0]
+
+
+def find_shift(sequence):
+    best_shift = 0
+    best_score = 0
+
+    # most used letters in english grammar, descending from ETA...
+    common_letters = "ETAOINSRHDLUCMFYWGPBVKJXQZ"
+
+    for shift in range(26):
+        # decrypt with this shift
+        decrypted_text= ""
+        for letter in sequence:
+            position = ord(letter) - ord('A') # converts letter to position
+            new_pos = (position - shift) % 26 #decrypts by subtracting the shift
+
+            decrypted_letter = chr(new_pos + ord('A')) ## converts back to letter
+            decrypted_text += decrypted_letter
+
+        score = 0
+        for letter in decrypted_text:
+            letter_position = common_letters.find(letter) # position in common-letters
+
+            if letter_position != -1:
+                letter_value = 26 - letter_position # higher value based on position in common_letters, the most common used letters in the english language - 26 points for E, 25 T and so on.
+                score += letter_value
+
+        if score > best_score:
+            best_score = score
+            best_shift = shift
+
+    return best_shift
+
+    # finds key based on probable key-length
+def find_key(ciphertext, key_length):
+    key = ""
+
+    for character in range(key_length):
+
+        sequence = []
+        c = character
+
+        while c < len(ciphertext):
+            sequence.append(ciphertext[c])
+            c += key_length
+
+        # the most probable shift, by analyzing frequency of letters in sequence and returns the most probable shift
+        best_shift = find_shift(sequence)
+
+        # ASCII-value of a(65), base, + shift-value, converts back to letter
+        key_letter = chr(best_shift + ord('A'))
+        key += key_letter
+
+    return key
+
+
