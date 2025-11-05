424c300aa69aafc4d6b71bc61adac35dc5b0d028
NT-000 <nicolai.thue.reier@gmail.com>
2025-11-05 11:36:59 +0100

    added Tailwind, navbar and components

--- Laravel/listings-board/resources/views/jobs/create.blade.php
+++ Laravel/listings-board/resources/views/jobs/create.blade.php
@@ -1,20 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create new job</title>
-</head>
-<body>
-<h1>Create new job!</h1>
-
-<form action="/jobs" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="Enter jobtitle">
-    <input type="text" name="info" placeholder="Enter description of job.">
-    <button type="submit">Submit form</button>
-</form>
-</body>
-</html>
+<x-layout>
+    <x-slot name="title">Create Job</x-slot>
+    <h1>Create new job</h1>
+    <form action="/jobs" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="Enter jobtitle">
+        <input type="text" name="info" placeholder="Enter description of job.">
+        <button type="submit">Submit form</button>
+    </form>
+</x-layout>
--- Laravel/listings-board/routes/web.php
+++ Laravel/listings-board/routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\JobController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('jobs', [JobController::class, 'index']);
+Route::resource('jobs', JobController::class);
 
-Route::get('jobs/create', [JobController::class, 'create']);
-
-Route::get('jobs/{id}', [JobController::class, 'show']);
-
-Route::post('jobs', [JobController::class, 'store']);
+//Route::resource('dashboard', DashboardController::class);
